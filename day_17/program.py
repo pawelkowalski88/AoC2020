884626cc04b260405e398f9d6af204a89e8cd98b
Pawel <88.pawel.kowalski@gmail.com>
2020-12-19 10:28:44 +0100

    day 17 part 2

--- day_17/program.py
+++ day_17/program.py
@@ -3,7 +3,7 @@ file = open('input.txt')
 lines = file.readlines()
 file.close()
 
-lines = [[[(0, x-1, y-1),v] for x,v in enumerate(l) if v != '\n'] for y,l in enumerate(lines)]
+lines = [[[(0,0, x-1, y-1),v] for x,v in enumerate(l) if v != '\n'] for y,l in enumerate(lines)]
 cubes = {x[0]:x[1] for x in list(itertools.chain(*lines))}
 
 
@@ -16,11 +16,12 @@ for i in range(6):
         for x in [-1,0,1]:
             for y in [-1,0,1]:
                 for z in [-1,0,1]:
-                    if cubes[c] == '#':
-                        key = (c[0]+x, c[1]+y, c[2]+z)
-                        if key not in neighbors.keys():
-                            neighbors[key] = 0
-                        neighbors[key] += 1 
+                    for w in [-1,0,1]:
+                        if cubes[c] == '#':
+                            key = (c[0]+x, c[1]+y, c[2]+z, c[3]+w)
+                            if key not in neighbors.keys():
+                                neighbors[key] = 0
+                            neighbors[key] += 1 
     new_cubes = {}
     for n in neighbors:
         if cubes.get(n, '.') == '.':
